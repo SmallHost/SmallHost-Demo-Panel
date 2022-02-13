dce2076e7e59d778b01b25dd31caa92b9ab1d9fc
SmallHost <94567339+SmallHost@users.noreply.github.com>
2022-02-13 07:28:21 -0500

    Update sitebuilder.js

--- sitebuilder.js
+++ sitebuilder.js
@@ -1,8 +1,8 @@
-var linkto = "https://html5up.net/",
-linktext = "HTML5 Up";
+var builderlinkto = "https://html5up.net/",
+builderlinktext = "HTML5 Up";
 var t = ["item_", "icon-"];
 for (n=0; n<=1; n++)
 {
-document.getElementById(`${t[n]}sitereptile`).setAttribute("href", linkto)
-document.getElementById(`${t[n]}sitereptile`).innerHTML=linktext;
+document.getElementById(`${t[n]}sitereptile`).setAttribute("href", builderlinkto)
+document.getElementById(`${t[n]}sitereptile`).innerHTML=builderlinktext;
 }
